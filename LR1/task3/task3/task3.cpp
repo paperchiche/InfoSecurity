f2072f4baa42a4cc32a946684b4875413173a9dd
paperchiche <20artyom02@mail.ru>
2022-09-27 00:37:00 +0300

    LR1 (task3) is done

---
+++ LR1/task3/task3/task3.cpp
@@ -0,0 +1,32 @@
+﻿#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    ifstream file_in("input.txt");
+    ofstream file_out("output.txt");
+    string s;
+
+    while (file_in)
+    {
+        string s;
+        getline(file_in, s);
+        string end;
+        if (s.size() != 0)
+        {
+            for (int i = s.size() - 1; i >= 0; i--) // начинаем отсчет с конца 
+            {
+                end.push_back(s[i]);
+            }
+        }
+        file_out << end << '\n';
+
+    }
+
+    file_out.close();
+    file_in.close();
+
+    system("pause");
+}
